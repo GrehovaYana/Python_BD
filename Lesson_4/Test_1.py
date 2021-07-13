689b9ef3266b72b3100e6927560e1dbbed211357
GrehovaYana <63998562+GrehovaYana@users.noreply.github.com>
2021-07-13 23:03:48 +0300

    Delete Lesson_4 directory

--- Lesson_4/Test_1.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-
-
-def employees_salary():
-    working_hours, rate_per_hour, reward = map(float, argv[1:])
-    print(f"Employees salary - {working_hours * rate_per_hour + reward}")
-
-employees_salary()
