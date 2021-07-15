cc49a71baf49382bc558a5da477fc82dbfd61999
GrehovaYana <63998562+GrehovaYana@users.noreply.github.com>
2021-07-16 01:04:55 +0300

    Delete Lesson_5 directory

--- Lesson_5/Task_3.py
+++
@@ -1,4 +0,0 @@
-with open("text_3.txt", "r", encoding="utf-8") as emp_list:
-    emp_dict = {line.split()[0]: float(line.split()[1]) for line in emp_list}
-    print(f"Salary lower than 20000 = {[el[0] for el in emp_dict.items() if el[1] < 20000]}\n"
-          f"Average salary: {sum(emp_dict.values()) / len(emp_dict)} ")
--- Lesson_5/Task_6.py
+++
@@ -1,6 +0,0 @@
-import string
-
-with open("text_6.txt", "r", encoding="utf-8") as final_dict:
-    final_marks = {string.split(":")[0]: sum([int(s[:s.index("(")]) for s in string.split() if "(" in s]) for string in
-                   final_dict}
-    print(final_marks)
