6411ef232fd5b806ec2d66dd92b44c07f6054abc
GrehovaYana <63998562+GrehovaYana@users.noreply.github.com>
2021-07-20 21:58:31 +0300

    Delete Lesson_5 directory

--- Lesson_5/Task_2.py
+++
@@ -1,3 +0,0 @@
-with open("text_1.txt", 'r', encoding="utf-8") as new_f:
-    inf_ = [f"{line}. {len(count.split())} words" for line, count in enumerate(new_f, 1)]
-    print(*inf_, f"{len(inf_)} strings", sep="\n")
--- Lesson_5/Task_7.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("text_77.json", "w") as write_f:
-    with open("text_7.txt", "r", encoding="utf-8") as f:
-        profit_dict = {line.split()[0]: int(line.split()[2]) - int(line.split()[3]) for line in f}
-        full_dict = [profit_dict, {"average_profit": sum([int(el) for el in profit_dict.values() if int(el) > 0]) / len(
-            [int(el) for el in profit_dict.values() if int(el) > 0])}]
-    json.dump(full_dict, write_f, ensure_ascii=False)
